2f6cab8b15bbb41f8e48d46915c33e632f6ee813
layesheva <layesheva@gmail.com>
2022-09-25 15:12:37 +0000

    cat align

--- components/CardCategorie.jsx
+++ components/CardCategorie.jsx
@@ -6,7 +6,7 @@ const CardCategorie = ({ data }) => {
     <>
       <Link href={`/`}>
         <a>
-          <div className="w-28 h-10 py-auto rounded-lg bg-yellow-700">
+          <div className="w-28 h-10 my-auto rounded-lg bg-yellow-700">
             {/* <img src="categorie.svg" alt="categorie" className="w-20 h-20 mx-auto"/> */}
             {/* <Image
               src="/logo.png"
@@ -15,7 +15,7 @@ const CardCategorie = ({ data }) => {
               height={100}
               layout="fixed"
             /> */}
-            <p className="text-center font-semibold text-lg text-white">{data.categorie}</p>
+            <p className="text-center py-1 font-semibold text-lg text-white">{data.categorie}</p>
           </div>
         </a>
       </Link>
